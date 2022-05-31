fdce08682332d58f6dc92e0f96d7002fbdf14b99
yuanzhouhui <yuanzhouhui@outlook.com>
2022-05-31 16:32:28 +0800

    feat:initial code

---
+++ src/main/java/com/bright/commoncode/CommonCodeApplication.java
@@ -0,0 +1,13 @@
+package com.bright.commoncode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bright/commoncode/CommonCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bright.commoncode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
